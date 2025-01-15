2b35492c11f3b2a4cbf82a08e7cef31a659eb77f
OdclMesbahHasan <mdmesbahhasanodcl@gmail.com>
2025-01-15 18:03:56 +0600

    User Service Impl

--- inventoryManagementSystem/src/main/java/com/mesbah/inventoryManagementSystem/services/impl/UserServiceImpl.java
+++ inventoryManagementSystem/src/main/java/com/mesbah/inventoryManagementSystem/services/impl/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.mesbah.inventoryManagementSystem.dtos.RegisterRequest;
 import com.mesbah.inventoryManagementSystem.dtos.Response;
 import com.mesbah.inventoryManagementSystem.dtos.UserDTO;
 import com.mesbah.inventoryManagementSystem.enums.UserRole;
+import com.mesbah.inventoryManagementSystem.exceptions.InvalidCredentialsException;
+import com.mesbah.inventoryManagementSystem.exceptions.NotFoundException;
 import com.mesbah.inventoryManagementSystem.models.User;
 import com.mesbah.inventoryManagementSystem.repositories.UserRepository;
 import com.mesbah.inventoryManagementSystem.security.JwtUtils;
@@ -12,8 +14,13 @@ import com.mesbah.inventoryManagementSystem.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -39,42 +46,116 @@ public class UserServiceImpl implements UserService {
         userRepository.save(userToSave);
         return Response.builder()
                 .status(200)
-                .message("User was succesfully registered")
+                .message("User was successfully registered")
                 .build();
     }
 
     @Override
     public Response loginUser(LoginRequest loginRequest) {
-        return null;
+        User user = userRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(() -> new NotFoundException("Email not found"));
+            if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+                throw new InvalidCredentialsException("Password doesn't match");
+            }
+        String token = jwtUtils.generateToken(user.getName());
+        return Response.builder()
+                .status(200)
+                .message("User logged in successfully")
+                .role(user.getRole())
+                .token(token)
+                .expirationTime("6 months")
+                .build();
+
     }
 
     @Override
     public Response getAllUsers() {
-        return null;
+        List<User> users = userRepository.findAll();
+        users.forEach(user -> user.setTransactions(null));
+        List<UserDTO> userDTOS = modelMapper.map(users, new TypeToken<List<UserDTO>>() {
+        }.getType());
+        return Response.builder()
+                .status(200)
+                .message("success")
+                .users(userDTOS)
+                .build();
     }
 
     @Override
     public Response getUserById(Long id) {
-        return null;
+        User user = userRepository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
+        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+        userDTO.setTransactions(null);
+        return Response.builder()
+                .status(200)
+                .message("success")
+                .user(userDTO)
+                .build();
     }
 
     @Override
     public User getCurrentLoggedInUser() {
-        return null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("User Not Found"));
+
+        user.setTransactions(null);
+
+        return user;
     }
 
     @Override
-    public Response updateUser(Long is, UserDTO userDTO) {
-        return null;
+    public Response updateUser(Long id, UserDTO userDTO) {
+
+        User existingUser = userRepository.findById(id).orElseThrow(() -> new NotFoundException("User Not Found"));
+
+        if (userDTO.getEmail() != null) existingUser.setEmail(userDTO.getEmail());
+        if (userDTO.getPhoneNumber() != null) existingUser.setPhoneNumber(userDTO.getPhoneNumber());
+        if (userDTO.getName() != null) existingUser.setName(userDTO.getName());
+        if (userDTO.getRole() != null) existingUser.setRole(userDTO.getRole());
+
+        if (userDTO.getPassword() != null && !userDTO.getPassword().isEmpty()) {
+            existingUser.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        }
+        userRepository.save(existingUser);
+
+        return Response.builder()
+                .status(200)
+                .message("User successfully updated")
+                .build();
     }
 
     @Override
     public Response deleteUser(Long id) {
-        return null;
+        userRepository.findById(id).orElseThrow(() -> new NotFoundException("User Not Found"));
+
+        userRepository.deleteById(id);
+
+        return Response.builder()
+                .status(200)
+                .message("User successfully Deleted")
+                .build();
+
     }
 
     @Override
     public Response getUserTransactions(Long id) {
-        return null;
+
+        User user = userRepository.findById(id).orElseThrow(() -> new NotFoundException("User Not Found"));
+
+        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+
+        userDTO.getTransactions().forEach(transactionDTO -> {
+            transactionDTO.setUser(null);
+            transactionDTO.setSupplier(null);
+        });
+
+        return Response.builder()
+                .status(200)
+                .message("success")
+                .user(userDTO)
+                .build();
     }
 }
